51585afc2997d93621b63675e95d88dafe7412c5
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2023-08-17 11:32:34 +0100

    w1 hack

--- main.ts
+++ main.ts
@@ -11,9 +11,20 @@ function hit_floor (floor: Sprite) {
         bot.vy = 0
     }
 }
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (bot.overlapsWith(bottom)) {
+        bot.vy = -200
+    }
+    if (bot.overlapsWith(top)) {
+        bot.vy = 200
+    }
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (bot.overlapsWith(bottom)) {
         bot.vy = -150
+    }
+    if (bot.overlapsWith(top)) {
+        bot.vy = 150
     }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (bot, obstacle) {
@@ -22,6 +33,10 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (bot, obstacle) 
 function spawn_obstacle () {
     obstacle = sprites.create(assets.image`blue obstacle`, SpriteKind.Enemy)
     obstacle.bottom = 110
+    if (randint(1, 2) == 1) {
+        obstacle.setImage(assets.image`red obstacle`)
+        obstacle.top = 10
+    }
     obstacle.left = camera_right - 5
     last_spawn_x = obstacle.left
     obstacle.setFlag(SpriteFlag.AutoDestroy, true)
@@ -31,10 +46,15 @@ function move_camera () {
     camera_right = scene.cameraProperty(CameraProperty.Right)
 }
 function y_movement () {
-    bot.vy += gravity
+    if (bot.y > 60) {
+        bot.vy += gravity
+    } else {
+        bot.vy += gravity * -1
+    }
 }
 let obstacle: Sprite = null
 let gravity_direction = 0
+let top: Sprite = null
 let bot: Sprite = null
 let bottom: Sprite = null
 let camera_right = 0
@@ -43,7 +63,7 @@ let camera_offset = 0
 let gravity = 0
 gravity = 8
 let game_speed = 50
-let min_spawn_gap = 50
+let min_spawn_gap = 25
 camera_offset = 64
 last_spawn_x = 0
 camera_right = scene.cameraProperty(CameraProperty.Right)
@@ -56,6 +76,10 @@ bot.vx = game_speed
 bot.z = -1
 scene.setBackgroundColor(1)
 spawn_obstacle()
+top = sprites.create(assets.image`bottom`, SpriteKind.ground)
+top.image.fill(2)
+top.top = 0
+top.vx = game_speed
 game.onUpdate(function () {
     move_camera()
     if (camera_right - last_spawn_x > min_spawn_gap) {
@@ -64,6 +88,7 @@ game.onUpdate(function () {
         }
     }
     y_movement()
+    hit_floor(top)
     hit_floor(bottom)
     info.changeScoreBy(1)
 })
